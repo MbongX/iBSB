494d419891db17ca9d517b965bb4c8c9726470d1
RonaldTayie <1999roniengwe@gmail.com>
2023-04-16 23:31:53 +0200

    Backemd redo presave..

--- I Bank - Student Bank/Service/BankService.cs
+++ I Bank - Student Bank/Service/BankService.cs
@@ -11,6 +11,8 @@ namespace I_Bank___Student_Bank.Service
     public class BankService
     {
 
+        DateTime today = new DateTime().Date;
+
         List<AccountType> accountTypes = new List<AccountType>();
         List<TransactionType>transactionTypes = new List<TransactionType>();
         List<Account> accounts = new List<Account>();
@@ -35,7 +37,63 @@ namespace I_Bank___Student_Bank.Service
         List<AccountType> GetAccountTypes() => this.accountTypes;
         List<TransactionType> GetTransactionTypes() => this.transactionTypes;
 
+        public List<Account> GetNewAccounts() {
+            List<Account> new_acc = new List<Account>();
+            this.accounts.ForEach(account => {
+                Account acc = account;
+                if(acc.DateCreated.Date.Equals(this.today)){
+                    new_acc.Add(acc);
+                }
+            });
+            return new_acc;
+        }
 
+        public List<Account> GetNewAccountsMonth() {
+            List<Account> new_month = new List<Account>();
+            this.accounts.ForEach(acc=>{
+                Account account = acc;
+                if (account.DateCreated.Month.Equals(this.today.Month)) {
+                    new_month.Add(account);
+                }
+            });
+            return new_month;
+        }
+
+        // Will return new Account Balance after Deposit or withdrawal
+        public double Transact(int AccountId, double amount,bool isDeposit=true) {
+            // Is Account set
+            bool isAccountSet = false;
+            //Find account
+            foreach (Account account in this.accounts) {
+                if (account.Id.Equals(AccountId)) {
+                    Transaction transaction = new Transaction();
+                    double finalAmount = isDeposit?account.CurrentBalance + amount:account.CurrentBalance - amount;
+                    transaction.ClosingAmount = finalAmount;
+                    transaction.OpeningAmount = account.CurrentBalance;
+                    transaction.date = new DateTime().Date;
+                    transaction.AccId = AccountId;
+                    transaction.TransactionTypeId = 1;
+                    transaction.Amount = amount;
+                    account.CurrentBalance = finalAmount;
+                    // Add transactio to list
+                    this.transactions.Add(transaction);
+                    return finalAmount;
+                }
+
+            }
+            return 0.0;
+
+        }
+
+        public List<Transaction> getAccountTransactions(int AccountId) {
+            return (List<Transaction>)this.transactions.Where(t => t.AccId.Equals(AccountId));
+        }
+
+        public bool CreateAccount(String firstname,String lastname,int acc_type,double initialDeposit) {
+
+            return true;
+
+        }
 
     }
 }
